68ec45cf393171419ec30bcf6f60b2b00594a605
Norman Henges <normanhenges@proton.me>
2026-03-10 22:05:42 +0100

    refactor: Build settings ui via xml file

--- prefs.js
+++ prefs.js
@@ -24,143 +24,84 @@ import { ExtensionPreferences } from "resource:///org/gnome/Shell/Extensions/js/
 
 export default class HotEdgePreferences extends ExtensionPreferences {
     fillPreferencesWindow(window) {
-        this.settings = this.getSettings();
+        const settings = this.getSettings();
 
-        const page = new Adw.PreferencesPage({
-            title: "General",
-            icon_name: "dialog-information-symbolic",
-        });
+        const builder = new Gtk.Builder();
+        const uiFilePath = this.path.concat("/ui/prefs.ui");
+        builder.add_from_file(uiFilePath);
+
+        const page = builder.get_object("preferences_main_page");
         window.add(page);
 
-        const positionGroup = new Adw.PreferencesGroup({
-            title: "Position",
-        });
-        page.add(positionGroup);
-
-        const behaviorGroup = new Adw.PreferencesGroup({
-            title: "Behavior",
-        });
-        page.add(behaviorGroup);
-
-        const appearanceGroup = new Adw.PreferencesGroup({
-            title: "Appearance",
-        });
-        page.add(appearanceGroup);
-
-        // edge-size
-        let edgeSizeAdjustment = new Gtk.Adjustment({
-            lower: 1,
-            upper: 100,
-            step_increment: 10,
-        });
-        this.buildSpinRow("edge-size", "Edge Size", "%", positionGroup, edgeSizeAdjustment);
-
-        // primary-monitor-only
-        const primaryMonitorOnlyRow = new Adw.SwitchRow({
-            title: "Only on Primary Monitor",
-        });
-        this.settings.bind("primary-monitor-only", primaryMonitorOnlyRow, "active", Gio.SettingsBindFlags.DEFAULT);
-        positionGroup.add(primaryMonitorOnlyRow);
-
-        if (this.settings.get_boolean("fallback-in-use")) {
-            // fallback-timeout
-            let timeoutAdjustment = new Gtk.Adjustment({
-                lower: 1,
-                upper: 1000,
-                step_increment: 50,
-            });
-            this.buildSpinRow("fallback-timeout", "Activation Timeout", "ms", behaviorGroup, timeoutAdjustment);
-        } else {
-            // pressure-threshold
-            let pressureAdjustment = new Gtk.Adjustment({
-                lower: 0,
-                upper: 500,
-                step_increment: 25,
-            });
-            this.buildSpinRow("pressure-threshold", "Activation Pressure", "px", behaviorGroup, pressureAdjustment);
-        }
-
-        // suppress-activation-when-button-held
-        const suppressWhenButtonHeldRow = new Adw.SwitchRow({
-            title: "Suppress on Mouse Button",
-            subtitle: "Don't activate overview while a mouse button is held",
-        });
-        this.settings.bind(
-            "suppress-activation-when-button-held",
-            suppressWhenButtonHeldRow,
-            "active",
-            Gio.SettingsBindFlags.DEFAULT,
-        );
-        behaviorGroup.add(suppressWhenButtonHeldRow);
-
-        // suppress-activation-when-fullscreen
-        const suppressWhenFullscreenRow = new Adw.SwitchRow({
-            title: "Suppress on Fullscreen",
-            subtitle: "Don't activate overview while an application is displayed in fullscreen mode",
-        });
-        this.settings.bind(
-            "suppress-activation-when-fullscreen",
-            suppressWhenFullscreenRow,
-            "active",
-            Gio.SettingsBindFlags.DEFAULT,
-        );
-        behaviorGroup.add(suppressWhenFullscreenRow);
-
-        // show-animation
-        const showAnimationRow = new Adw.SwitchRow({
-            title: "Show Animation when Activated",
-        });
-        this.settings.bind("show-animation", showAnimationRow, "active", Gio.SettingsBindFlags.DEFAULT);
-        appearanceGroup.add(showAnimationRow);
+        this.bindSettings(builder, settings);
     }
 
-    buildSpinRow(preferenceKey, title, unit, group, adjustment) {
-        const row = new Adw.ActionRow({
-            title: title,
-        });
-        row.add_css_class("spin");
+    bindSettings(builder, settings) {
+        // Position
+        settings.bind("edge-size",
+            builder.get_object("edge_size_spin_button"),
+            "value",
+            Gio.SettingsBindFlags.DEFAULT);
+        this.connectResetButton("edge-size",
+            builder.get_object("edge_size_reset_button"),
+            settings);
+        settings.bind("primary-monitor-only",
+            builder.get_object("primary_monitor_only_switch"),
+            "active",
+            Gio.SettingsBindFlags.DEFAULT);
 
-        const box = new Gtk.Box({
-            orientation: Gtk.Orientation.HORIZONTAL,
-        });
-        row.add_suffix(box);
+        // Behavior
+        settings.bind("fallback-timeout",
+            builder.get_object("timeout_spin_button"),
+            "value",
+            Gio.SettingsBindFlags.DEFAULT);
+        this.connectResetButton("fallback-timeout",
+            builder.get_object("timeout_reset_button"),
+            settings);
+        settings.bind("pressure-threshold",
+            builder.get_object("pressure_spin_button"),
+            "value",
+            Gio.SettingsBindFlags.DEFAULT);
+        this.connectResetButton("pressure-threshold",
+            builder.get_object("pressure_reset_button"),
+            settings);
+        settings.bind("suppress-activation-when-button-held",
+            builder.get_object("suppress_on_mouse_switch"),
+            "active",
+            Gio.SettingsBindFlags.DEFAULT);
+        settings.bind("suppress-activation-when-fullscreen",
+            builder.get_object("suppress_on_fullscreen_switch"),
+            "active",
+            Gio.SettingsBindFlags.DEFAULT);
 
-        const resetButton = new Gtk.Button({
-            icon_name: "view-refresh-symbolic",
-            tooltip_text: "Reset to default",
-            valign: Gtk.Align.CENTER,
-            hexpand: false,
-            vexpand: false
+        // Decide whether to show timeout or pressure
+        const fallbackInUse = settings.get_boolean("fallback-in-use");
+        builder.get_object("timeout_row").connect("map", () => {
+            button.visible = fallbackInUse;
         });
-        resetButton.add_css_class("flat");
-        resetButton.connect("clicked", () => {
-            this.settings.reset(preferenceKey);
-        });
-        resetButton.connect("map", () => {
-            resetButton.visible =
-                this.settings.get_uint(preferenceKey) != this.settings.get_default_value(preferenceKey).get_uint32();
-        });
-        this.settings.connect("changed::".concat(preferenceKey), () => {
-            resetButton.visible =
-                this.settings.get_uint(preferenceKey) != this.settings.get_default_value(preferenceKey).get_uint32();
+        builder.get_object("pressure_row").connect("map", () => {
+            button.visible = !fallbackInUse;
         });
 
-        box.append(resetButton);
-        const spinner = new Gtk.SpinButton({
-            adjustment: adjustment,
-        });
-        this.settings.bind(preferenceKey, spinner, "value", Gio.SettingsBindFlags.DEFAULT);
-        row.activatable_widget = spinner;
-        box.append(spinner);
-
-        const unitLabel = new Gtk.Label({
-            label: unit,
-            width_chars: "3",
-            xalign: 1.0,
-            justify: Gtk.Justification.RIGHT,
-        });
-        box.append(unitLabel);
-
-        group.add(row);
+        // Appearance
+        settings.bind("show-animation",
+            builder.get_object("show_animation_switch"),
+            "active",
+            Gio.SettingsBindFlags.DEFAULT);
     }
+
+    connectResetButton(preferenceKey, button, settings) {
+        button.connect("clicked", () => {
+            settings.reset(preferenceKey);
+        });
+        button.connect("map", () => {
+            button.visible =
+                settings.get_uint(preferenceKey) != settings.get_default_value(preferenceKey).get_uint32();
+        });
+        settings.connect("changed::".concat(preferenceKey), () => {
+            button.visible =
+                settings.get_uint(preferenceKey) != settings.get_default_value(preferenceKey).get_uint32();
+        });
+    }
+
 }
